048e8a741d0c761490a052438736501d72a08723
Liew Yong Hong <149985933+yonghongzz@users.noreply.github.com>
2024-10-31 23:00:44 +0800

    merge 2 branch (#6)

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'main-acc-tr/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
