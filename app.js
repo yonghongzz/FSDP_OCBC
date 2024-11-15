c60fa200385f145d17da6c1c5a2e098530940603
yonghongzz <s10257564@connect.np.edu.sg>
2024-11-15 22:47:02 +0800

    Make start page login page

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
 
